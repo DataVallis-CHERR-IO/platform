07addceee672987f08817a875116f6e70710a183
davidtacer <david.tacer@data-vallis.com>
2022-11-10 15:50:50 +0100

    front updates

--- src/views/home/top-features.tsx
+++ src/views/home/top-features.tsx
@@ -26,13 +26,7 @@ const TopFeatures: React.FC = () => {
                   <p className='text-muted mb-0'>{t('home.topFeatures.text1.subtitle')}</p>
                 </div>
               </div>
-              <div className='col-lg-6 col-md-6 col-xl-4'>
-                <div className='mt-5'>
-                  <FontAwesomeIcon icon={faMedal} className='text-primary mb-4' size='2x' />
-                  <h3 className='h4 mb-2'>{t('home.topFeatures.text2.title')}</h3>
-                  <p className='text-muted mb-0'>{t('home.topFeatures.text2.subtitle')}</p>
-                </div>
-              </div>
+
               <div className='col-lg-6 col-md-6 col-xl-4'>
                 <div className='mt-5'>
                   <FontAwesomeIcon icon={faUsers} className='text-primary mb-4' size='2x' />
@@ -49,6 +43,28 @@ const TopFeatures: React.FC = () => {
               </div>
               <div className='col-lg-6 col-md-6 col-xl-4'>
                 <div className='mt-5'>
+                  {<FontAwesomeIcon icon={faVrCardboard} className='text-primary mb-4' size='2x' />}
+                  <h3 className='h4 mb-2'>{t('home.topFeatures.text6.title')}</h3>
+                  <p className='text-muted mb-0'>{t('home.topFeatures.text6.subtitle')}</p>
+                </div>
+              </div>
+              <div className='col-lg-6 col-md-6 col-xl-4'>
+                <div className='mt-5'>
+                  {<FontAwesomeIcon icon={faVrCardboard} className='text-primary mb-4' size='2x' />}
+                  <h3 className='h4 mb-2'>{t('home.topFeatures.text7.title')}</h3>
+                  <p className='text-muted mb-0'>{t('home.topFeatures.text7.subtitle')}</p>
+                </div>
+              </div>
+
+              <div className='col-lg-6 col-md-6 col-xl-4'>
+                <div className='mt-5'>
+                  {<FontAwesomeIcon icon={faVrCardboard} className='text-primary mb-4' size='2x' />}
+                  <h3 className='h4 mb-2'>{t('home.topFeatures.text8.title')}</h3>
+                  <p className='text-muted mb-0'>{t('home.topFeatures.text8.subtitle')}</p>
+                </div>
+              </div>
+              <div className='col-lg-6 col-md-6 col-xl-4'>
+                <div className='mt-5'>
                   {<FontAwesomeIcon icon={faChartLine} className='text-primary mb-4' size='2x' />}
                   <h3 className='h4 mb-2'>{t('home.topFeatures.text5.title')}</h3>
                   <p className='text-muted mb-0'>{t('home.topFeatures.text5.subtitle')}</p>
@@ -56,9 +72,9 @@ const TopFeatures: React.FC = () => {
               </div>
               <div className='col-lg-6 col-md-6 col-xl-4'>
                 <div className='mt-5'>
-                  {<FontAwesomeIcon icon={faVrCardboard} className='text-primary mb-4' size='2x' />}
-                  <h3 className='h4 mb-2'>{t('home.topFeatures.text6.title')}</h3>
-                  <p className='text-muted mb-0'>{t('home.topFeatures.text6.subtitle')}</p>
+                  <FontAwesomeIcon icon={faMedal} className='text-primary mb-4' size='2x' />
+                  <h3 className='h4 mb-2'>{t('home.topFeatures.text2.title')}</h3>
+                  <p className='text-muted mb-0'>{t('home.topFeatures.text2.subtitle')}</p>
                 </div>
               </div>
             </div>
